96b56a9bd12f41df313eebe459b0b787ece1532c
abhishek <abhishekmandal0097@gmail.com>
2018-01-01 20:18:40 +0530

    Inital Commit

--- src/Generics/demo5.java
+++ src/Generics/demo5.java
@@ -2,19 +2,25 @@ package Generics;
 
 /**
  * This program shows how to implement a generic method in java
+ * In this method you can pass any number of arguments with any type
+ * and can also pass arrays to it.
  *
  */
 import java.util.*;
 public class demo5 {
-    private static<T> void   display(T ob)
+    private static <T> void debug(T... ob)
     {
-        System.out.println(ob.getClass().getName()+" "+ob);
+        System.out.println(Arrays.deepToString(ob));
 
     }
     public static void main(String args[])
     {
-        display(11);
-        display("hello world");
-        display(233.33);
+        debug(11);
+        debug("hello world");
+        debug(233.33);
+        debug('a','b');
+        debug("Abhishek","Mandal");
+        debug(new int[]{1,2,3,4,5});
+
     }
 }
