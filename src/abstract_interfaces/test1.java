7b322cd6cce8708ad9dae85db9000d59fcb95a17
abhishek <abhishekmandal0097@gmail.com>
2018-01-20 11:06:21 +0530

    Simple interface example

--- src/abstract_interfaces/test1.java
+++ src/abstract_interfaces/test1.java
@@ -1,7 +1,9 @@
 package abstract_interfaces;
+
 /*
 this code shows what is an interface and how do we implement it
  */
+
  interface interfacedemo
 {
     /*
@@ -15,21 +17,17 @@ interface goit
 {
     void whatsup();
 }
-abstract class voidsum implements interfacedemo
-{
-    void sumit()
-    {
 
-    }
-}
 interface getString
 {
     default String getString()
     {
-        return "gennady korotkeivich";
+        return "i am in getstring method";
     }
 }
-public class test1 implements interfacedemo,goit{
+
+public class test1 implements interfacedemo,goit
+{
      public void whatsup()
      {
          System.out.println("hello there abhishek mandal");
@@ -39,18 +37,13 @@ public class test1 implements interfacedemo,goit{
     {
         System.out.println("hello how are you");
     }
+
     public static void main(String args[])
     {
             System.out.println(a);
             test1 ob1=new test1();
-            interfacedemo ob2=ob1;  //reference variable of the first interface
-            ob2.demo();
+            ob1.demo();
             System.out.println(test1.a);  //as the variable is final and static it can be accessed by the classname
-
-        //creating reference variable of interface two
-          goit ob3=ob1;
-          ob3.whatsup();
-
-
+            ob1.whatsup();
     }
 }
