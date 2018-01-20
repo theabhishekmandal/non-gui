d5d0bec0d718552d353e16c0306f238342524339
abhishek <abhishekmandal0097@gmail.com>
2018-01-20 18:38:52 +0530

    Collections example

--- src/Collection_demo/arraylistdemo.java
+++ src/Collection_demo/arraylistdemo.java
@@ -1,7 +1,8 @@
 package Collection_demo;
 
 import java.util.*;
-public class arraylistdemo {
+public class arraylistdemo
+{
     public static void main(String args[])
     {
         ArrayList<Object> arr=new ArrayList<>();
@@ -9,6 +10,16 @@ public class arraylistdemo {
         arr.add("hello");
         arr.add(12.00000000);
 
+        //Storing an integer to an ArrayList
+        ArrayList<Integer> foo=new ArrayList<Integer>();
+        foo.add(4);
+        foo.add(5);
+        foo.add(1);
+        foo.add(2);
+        foo.add(3);
+        foo.add(7);
+        foo.add(8);
+        Collections.sort(foo);
         ListIterator it=arr.listIterator();
         StringBuilder st=new StringBuilder();
         while(it.hasNext())
