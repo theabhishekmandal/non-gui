a3dfd4dee4e3bc075c642e60070adb782ec99705
abhishek <abhishekmandal0097@gmail.com>
2018-02-28 09:33:14 +0530

    initial commit

--- src/Lambda_Expressions/demo6.java
+++ src/Lambda_Expressions/demo6.java
@@ -1,8 +1,37 @@
 package Lambda_Expressions;
 
-public class demo6 {
-    public static void main(String args[])
-    {
+/**
+ * This program is an example of empty array exception. If the lambda expression throws a checked exception
+ * then the exception must declare in the abstract method of the functional interface.
+ */
 
+interface DoubleNumericArrayFunc{
+    double func(double[] n) throws EmptyArrayException;
+}
+
+class EmptyArrayException extends Exception{
+    EmptyArrayException(){
+        super("empty array");
+    }
+}
+public class demo6 {
+    public static void main(String args[]) throws EmptyArrayException{
+        double[] values = {1.0, 2.0, 3.0 ,4.0};
+
+        // This block is finding the average of double values
+        DoubleNumericArrayFunc average = (n) ->{
+            double sum = 0;
+
+            if(n.length == 0) throw new EmptyArrayException();
+
+            for(int i = 0 ; i < n.length ; i++){
+                sum += n[i];
+            }
+            return sum / n.length;
+        };
+        System.out.println("The average is " + average.func(values));
+
+        // This causes the exception to be thrown
+        System.out.println("The average is " + average.func(new double[0]));
     }
 }
