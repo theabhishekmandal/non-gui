20828777fb70b6a80faed0a3885711ead5295ba1
abhishek <abhishekmandal0097@gmail.com>
2018-03-07 18:20:49 +0530

    fixes

--- src/Lambda_Expressions/demo8.java
+++ src/Lambda_Expressions/demo8.java
@@ -1,13 +1,16 @@
 package Lambda_Expressions;
 
 /**
- *  This is an example of METHOD REFERENCE to STATIC METHODS using lambda expressions
+ *  This is an example of METHOD REFERENCE to STATIC METHODS using lambda expressions.
  *
  *  A method reference provides a way to refer to a method without executing it i.e,
  *  if there is an interface with a single abstract method then we can assign a static method
  *  to the lambda expression so as to create the body of the lambda expression
  *
  *  When evaluated, a method reference also creates an instance of the functional interface.
+ *
+ *  The General Syntax is given by:
+ *                                  Classname::methodName
  */
 
 // a functional interface for string operations
