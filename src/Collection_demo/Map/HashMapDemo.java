f8bc78de9408b0abf4a3038b7a4dc5ef15929391
abhishek <abhishekmandal0097@gmail.com>
2018-09-27 08:10:28 +0530

    Collections Implementation

---
+++ src/Collection_demo/Map/HashMapDemo.java
@@ -0,0 +1,35 @@
+package Collection_demo.Map;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+/**
+ *  HashMap extends AbstractMap and implements the Map interface.
+ *  A map is an object that stores associations between key value pairs.
+ *  Given a key you can find its value.Both keys and values are objects.
+ *  The keys must be unique but the values can be duplicated.
+ *  Some maps can accept null values and null keys while some cannot.
+ *  Also Maps don't implement the Iterable interface i.e you cannot iterate through a map using a for-each
+ *  for  loop
+ *  The order of the elements is not stored in hashMap
+ */
+
+public class HashMapDemo {
+    public static void main(String[] args) {
+        HashMap<String , String> arr = new HashMap<>();
+        arr.put("Abhishek", "Mandal");
+        arr.put("hello", "world");
+        arr.put("Mangekyo", "Sharingan");
+        arr.put("Rasen", "Shuriken");
+        arr.put("Taijuu", "kage bunshinn jutsu");
+
+        // Getting the collections view of the map as map does not implements collections interface
+        // entrySet() method provides a set of key value pairs
+        Set<Map.Entry<String, String>> set = arr.entrySet();
+
+        // now looping and displaying the keys and values
+        for(Map.Entry<String, String> ob : set)
+            System.out.println("Key : " + ob.getKey() + " Value:" + ob.getValue());
+    }
+}
