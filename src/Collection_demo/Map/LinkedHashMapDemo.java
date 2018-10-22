4cd3eb9f62946caaa237ae42f5ad6ab1a581ce3e
abhishek <abhishekmandal0097@gmail.com>
2018-10-22 08:23:52 +0530

    Collections Implementation

---
+++ src/Collection_demo/Map/LinkedHashMapDemo.java
@@ -0,0 +1,13 @@
+package Collection_demo.Map;
+
+/**
+ * LinkedHashMap extends HashMap. It maintains a linked list of the entries in the map, in the order in which they
+ * were inserted.
+ * The elements will be returned in the same way as they were inserted into the map.
+ * You can also create a LinkedHashMap that returns its elements in the order in which they were last accessed.
+ */
+public class LinkedHashMapDemo {
+    public static void main(String[] args) {
+
+    }
+}
