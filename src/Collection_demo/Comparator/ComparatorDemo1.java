ed9b76227c8f797aad1262953633735f6fc3c0fb
abhishek <abhishekmandal0097@gmail.com>
2018-10-28 23:33:05 +0530

    Collections Implementation

---
+++ src/Collection_demo/Comparator/ComparatorDemo1.java
@@ -0,0 +1,95 @@
+package Collection_demo.Comparator;
+import java.util.*;
+
+/**
+ * Compartor is particularly used for ordering of elements in different way.
+ * Comparator is particularly used for customized sorting order.
+ * Whereas comparable is used for natural order sorting order.
+ * By default the elements that are stored in natural order of insertion.
+ * TreeSet and TreeMap particularly used comparator to store elements in sorted order.
+ * Comparator is generic interface that has this declaration:
+ * 			interface Comparator<T> 	Here T specifies the type of objects being compared
+ * 
+ * It has the following methods:
+ * 
+ * 1 int compare(T obj1, T obj2)		:	returns zero if the two objects are equal,
+ * 											returns positive if obj1 is greater than obj2
+ * 											returns negative if obj1 is smaller than obj2
+ * 
+ * 2 boolean equals(object obj)			:	returns true if the invoking object is of the same type of obj
+ * 											else it returns false.
+ * 
+ * 3 default Comparator<T> reversed()	:	returns a comparator that reverses the ordering of the comparator on which
+ * 											it is called
+ *
+ * 4 static <T extends Comparable
+ *   <? super T>> Comparator<T>
+*   reverseOrder()						:	It returns a comparator that reverses the natural ordering of the elements
+ */
+public class ComparatorDemo1 {
+
+	public static void main(String[] args) {
+
+		// Creating a TreeSet in which elements will be inserted in descending order
+		// passing a comparator object and thereby implementing the compare method
+		// for customized sorting order.
+		TreeSet<Integer> arr = new TreeSet<>(new Comparator<Integer>(){
+			@Override
+			public int compare(Integer one, Integer two){
+				return -one.compareTo(two);
+			}
+		});
+		for(int i = 1; i < 11; i++) arr.add(i);
+		System.out.println(arr);
+
+
+		// implementing the reversed method
+		// first creating a comparator for natural sorting order
+		// then using reversed method to return a reverse comparator and sorting in descending order
+		Comparator<String> comparator = new Comparator<String>() {
+			@Override
+			public int compare(String o1, String o2) {
+				return o1.compareTo(o2);
+			}
+		};
+
+		ArrayList<String> hel = new ArrayList<>();
+		for(char ch = 'a'; ch <= 'z'; ch++) hel.add(ch + "");
+		Collections.sort(hel, comparator.reversed());
+		System.out.println(hel);
+
+
+		// implementing the reverseOrder method
+		ArrayList<City> helobj = new ArrayList<>();
+		helobj.add(new City("Delhi"));
+		helobj.add(new City("Mumbai"));
+		helobj.add(new City("Kolkata"));
+		helobj.add(new City("Chennai"));
+
+		/*
+		 This Comparator type can be replaced by Comparator.comparing
+		 Comparator<City>  cityComparator = (City one, City two) -> {
+		    return one.getName().compareTo(two.getName());
+		 };
+		*/
+		//first getting the comparator using Comparator.comparing and then reversing it with reverseOrder method
+		Comparator<City> cityComparator = Collections.reverseOrder(Comparator.comparing(City::getName));
+		Collections.sort(helobj, cityComparator);
+		System.out.println(helobj);
+	}
+}
+
+
+class City{
+	private String name;
+	City(String name){
+		this.name = name;
+	}
+	public String getName(){
+		return this.name;
+	}
+	@Override
+	public String toString(){
+		return "[" + this.name + "]";
+	}
+}
---
+++ src/Collection_demo/Comparator/ComparatorDemo2.java
@@ -0,0 +1,107 @@
+package Collection_demo.Comparator;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.TreeSet;
+
+/**
+ * This is an implementation on how differently a comparator is defined
+ * Also, different methods defined in comparator interface
+ *
+ * 1 static <T> Comparator<T>
+ *   nullsFirst(Comparator
+ *   <? super T> comp)                  :   this returns a comparator that views null values as less than
+ *                                          other values
+ *
+ * 2 static <T> Comparator<T>
+ *   nullsLast(Comparator
+ *   <? super T> comp)                  :   this returns a comparator that vies null values as greater than
+ *                                          other values
+ */
+
+// A Student class implementing the Comparator interface to sort in descending order
+class Student{
+    private String name;
+    private int number;
+
+    Student(String name, int number){
+        this.name = name;
+        this.number = number;
+    }
+
+    public String getname(){
+        return this.name;
+    }
+
+    public int getnumber(){
+        return this.number;
+    }
+    @Override
+    public String toString(){
+        return "[" + this.getnumber() + " " + this.getname()  + "]";
+    }
+}
+
+public class ComparatorDemo2 {
+    public static void main(String[] args) {
+
+        // First way of Defining a comparator using lambda expression
+        // Remember, to Store a user defined class object, TreeSet should
+        // define its comparator while declaring it
+        // defining the comparator to store elements in descending order
+        // this comparator will sort them on the basis of Integer values
+        TreeSet<Student> student = new TreeSet<>((Student one, Student two) -> {
+            Integer valone = one.getnumber();
+            Integer valtwo = two.getnumber();
+            return -valone.compareTo(valtwo);
+        });
+        student.add(new Student("Mokton", 1));
+        student.add(new Student("Doton", 2));
+        student.add(new Student("Raiton", 3));
+        student.add(new Student("Shuton", 4));
+        student.add(new Student("Fuuton", 5));
+        student.add(new Student("Khaton", 6));
+        System.out.println(student);
+
+
+        // Second way of defining a comparator separately to store the elements in descending order
+        // Without Lambda Expression
+        // Sorting based on String values
+        Comparator<Student> studentcomparator1 = new Comparator<Student>() {
+            @Override
+            public int compare(Student o1, Student o2) {
+                return o2.getname().compareTo(o1.getname());
+            }
+        };
+        TreeSet<Student> student2 = new TreeSet<>(studentcomparator1);
+        student2.add(new Student("Mokton", 1));
+        student2.add(new Student("Doton", 2));
+        student2.add(new Student("Raiton", 3));
+        student2.add(new Student("Shuton", 4));
+        student2.add(new Student("Fuuton", 5));
+        student2.add(new Student("Khaton", 6));
+        System.out.println(student2);
+
+
+        // Third way of defining a comparator
+        Comparator<Student> studentComparator2 = Comparator.comparing(Student::getnumber);
+        ArrayList<Student> stud = new ArrayList<>();
+        stud.add(new Student("Delhi", 1));
+        stud.add(new Student("Mumbai", 2));
+        stud.add(new Student("Chennai", 3));
+        stud.add(new Student("kolkata", 4));
+        stud.add(new Student("kanpur", 5));
+        Collections.sort(stud, Collections.reverseOrder(studentComparator2));
+        System.out.println(stud);
+
+        //implementing nullsFirst method
+        stud.add(null);
+        stud.sort(Comparator.nullsFirst(studentComparator2));
+        System.out.println(stud);
+
+        //implementing nullsLast method
+        stud.sort(Comparator.nullsLast(studentComparator2));
+        System.out.println(stud);
+
+    }
+}
