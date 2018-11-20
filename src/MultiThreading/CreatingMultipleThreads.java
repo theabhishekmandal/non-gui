e7b56ad566fbe5291d913db17a0de43514132bcc
abhishek <abhishekmandal0097@gmail.com>
2018-11-20 08:40:50 +0530

    MultiThreading fixes

--- src/MultiThreading/CreatingMultipleThreads.java
+++ src/MultiThreading/CreatingMultipleThreads.java
@@ -1,30 +1,21 @@
 package MultiThreading;
-/*
-In this program we are creating multi threads by implementing runnable interface
-Also in this we create two threads by using two objects
-means every object has at most one thread
+/**
+ * In this program we are creating multi threads by implementing runnable interface
+ * Also in this we create two threads by using two objects means every object has at most one thread
  */
 public class CreatingMultipleThreads implements Runnable {
-    Thread t;
-    String name;
-    CreatingMultipleThreads(String s)
-    {
-        t=new Thread(this,s);
-        t.start();
+    private Thread thread;
+    private String name;
+    CreatingMultipleThreads(String s) {
+        thread = new Thread(this,s);
+        thread.start();
     }
-    public void run()
-    {
-        for(int i=0;i<5;i++)
-
-            System.out.println("this is the value of the current thread "+Thread.currentThread().getName()+" "+i);
-
+    public void run(){
+        for(int i = 0; i < 5; i++)
+            System.out.println("this is the value of the current thread " + Thread.currentThread().getName() + " " + i);
     }
-    public static void main(String args[])
-    {
-        CreatingMultipleThreads ob1=new CreatingMultipleThreads("A");
-        CreatingMultipleThreads ob2=new CreatingMultipleThreads("B");
-
-
-
+    public static void main(String args[]) {
+        CreatingMultipleThreads ob1 = new CreatingMultipleThreads("A");
+        CreatingMultipleThreads ob2 = new CreatingMultipleThreads("B");
     }
 }
