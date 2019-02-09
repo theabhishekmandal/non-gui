41538c33a580776419e13bd65f83f516a2685d10
abhishek <abhishekmandal0097@gmail.com>
2019-02-09 16:39:50 +0530

    New Changes

--- src/CollectionImplementation/Comparator/ComparatorDemo1.java
+++ src/CollectionImplementation/Comparator/ComparatorDemo1.java
@@ -1,43 +1,6 @@
 package CollectionImplementation.Comparator;
 import java.util.*;
 
-/**
- * Comparator is particularly used for ordering of elements in different way.
- * Comparator is particularly used for customized sorting order.
- * Whereas comparable is used for natural order sorting order.
- * By default the elements that are stored in natural order of insertion.
- * TreeSet and TreeMap particularly used comparator to store elements in sorted order.
- * Comparator is generic interface that has this declaration:
- * 			interface Comparator<T> 	Here T specifies the type of objects being compared
- * 
- * It has the following methods:
- * 
- * 1 int compare(T obj1, T obj2)		:	returns zero if the two objects are equal,
- * 											returns positive if obj1 is greater than obj2
- * 											returns negative if obj1 is smaller than obj2
- *
- * 2 boolean equals(object obj)			:	returns true if the invoking object is of the same type of obj
- * 											else it returns false.
- * 
- * 3 default Comparator<T> reversed()	:	returns a comparator that reverses the ordering of the comparator on which
- * 											it is called
- *
- * 4 static <T extends Comparable
- *   <? super T>> Comparator<T>
- *   reverseOrder()						:	It returns a comparator that reverses the natural ordering of the elements
- *
- * 5 static<T, U extends Comparable
- * 	 <? super U>> Comparator<T>
- * 	 comparing(Function<? super T,
- * 	 ? extends U> getkey)				:	it returns a comparator of the given type
- *
- * 6 static <T,U> Comparator<T> comparing
- *  (Function<? super T,? extends U>
- *  keyExtractor, Comparator<? super U>
- *  keyComparator)						:	it returns a comparator of the given type, but allows overriding the
- *  										natural order of the sort key by providing the comparator that creates
- *  										custom ordering for the sort key
- */
 public class ComparatorDemo1 {
 
 	public static void main(String[] args) {
