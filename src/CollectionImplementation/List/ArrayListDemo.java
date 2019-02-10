d693158117557ddba1a13062e7e353e3446cadea
abhishek <abhishekmandal0097@gmail.com>
2019-02-11 01:46:34 +0530

    list constructors updated

--- src/CollectionImplementation/List/ArrayListDemo.java
+++ src/CollectionImplementation/List/ArrayListDemo.java
@@ -17,7 +17,6 @@ import static java.lang.System.out;
  *                                          elements of the collection c.
  *
  *  3 ArrayList(int capacity)           :   builds an array list that has the specified initial capacity.
- *
  */
 public class ArrayListDemo
 {
