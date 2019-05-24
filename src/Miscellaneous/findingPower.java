6a43932e1769ff19260801f9768204a30ca71a3c
theabhishekmandal <abhishekmandal0097@gmail.com>
2019-05-24 10:52:54 +0530

    updated pow

--- src/Miscellaneous/findingPower.java
+++ src/Miscellaneous/findingPower.java
@@ -12,7 +12,7 @@ public class findingPower {
 
     private static int pow(int x, int y) {
         if(y == 0) return 1;
-        if( y % 2 == 0)
+        if((y & 1) == 0)
             return pow(x, y /2) * pow(x, y / 2);
         else
             return pow(x, y / 2) * pow(x, y / 2) * x;
