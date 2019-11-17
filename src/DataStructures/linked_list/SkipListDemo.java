6d5af4b53add31f8f0b7439e1fb1673f31950735
theabhishekmandal <abhishekmandal0097@gmail.com>
2019-11-17 14:01:49 +0530

    Updated documentation of skipList 17Nov2019

--- src/DataStructures/linked_list/SkipListDemo.java
+++ src/DataStructures/linked_list/SkipListDemo.java
@@ -1,5 +1,27 @@
 package DataStructures.linked_list;
-
+/**
+ * This is an implementation of SkipList.
+ * -    In this randomised layers with fast pointers are used for searching the node.
+ * -    Each node has two pointers next and down as shown below
+ * -    Also each layer have level starting from max level to level 0
+ * -    A node can only be added if it's level is greater than or equal to current level
+ * -    Randomness is used to decide from which layer a node is going to be added
+ *
+ * For searching
+ * -    A node to be searched in the topmost layer
+ * -    we check while traversing if traversed node does not overshoot the value
+ *      of the node that is being searched
+ * -    If it overshoots then we move down a layer and search in that layer
+ * -    The same process is repeated again and again until the value is found.
+ *
+ * Null----------------> level 3
+ *  \
+ * Null---1--------4---> level 2
+ *   \    \        \
+ * Null---1-----3--4---> level 1
+ *   \    \     \  \
+ * Null---1--2--3--4---> level 0
+ */
 import java.util.Random;
  class SkipList<T extends Comparable<? super T>, U>{
     private class Node{
@@ -22,6 +44,8 @@ import java.util.Random;
                     "key=" + key +
                     ", value=" + value +
                     ", level=" + level +
+                    ", next=" + "[" + this.next + "]" +
+                    ", down=" + "[" + this.down + "]" +
                     '}';
         }
     }
@@ -47,12 +71,22 @@ import java.util.Random;
     }
     public void add(T key, U value){
         long level = level();
+        // if level is greater than create an upper layer
         if(level > this.head.level){
             head = new Node(null, null, level, null, this.head);
         }
         Node cur = this.head;
         Node last = null;
         while(cur != null){
+            /**
+             * If the next node is null or the next node's key is greater than
+             * current key, then only check if it can be added with current level
+             *
+             * Otherwise if node's key is similar to that of current key
+             * then just update the value
+             *
+             * else go for next node
+             */
             if(cur.next == null || cur.next.key.compareTo(key) > 0){
                 if(level >= cur.level){
                     Node n = new Node(key, value,cur.level, cur.next, null);
@@ -80,6 +114,13 @@ import java.util.Random;
         U value  = null;
         Node cur = this.head;
         while(cur != null){
+            /**
+             * if the next node is null or next node is greater than equal to
+             * then only you can remove the node. Also, remove it corresponding
+             * below node
+             *
+             * Otherwise check for next node
+             */
             if(cur.next == null || cur.next.key.compareTo(key) >= 0){
                 if(cur.next != null && cur.next.key.equals(key)){
                     value  = cur.next.value;
@@ -96,6 +137,15 @@ import java.util.Random;
     public U get(T key){
         Node cur = head;
         while(cur != null){
+            /**
+             * For searching the node start from upper layer,
+             * if the next node is null or next node's key is greater than
+             * current node then go to below node.
+             *
+             * Otherwise if the node is found then return it
+             *
+             * else continue to next node
+             */
             if(cur.next == null || cur.next.key.compareTo(key) > 0){
                 cur = cur.down;
                 continue;
@@ -110,9 +160,9 @@ import java.util.Random;
 public class SkipListDemo{
     public static void main(String[] args) {
         SkipList<Integer, Integer> s = new SkipList<>();
-        s.add(1, 100);
-        s.add(1, 200);
-        s.add(1, 300);
+        s.add(1, 2);
+        s.add(3, 2);
+        s.add(1, 2);
         System.out.println(s.get(1));
     }
 }
