8533bda59ee71a899cd83cde3fcab22c214c12d3
theabhishekmandal <abhishekmandal0097@gmail.com>
2019-12-27 12:15:09 +0530

    adding comments on reverseKnodes class 27Dec2019

--- src/DataStructures/linked_list/Problems/ReverseKNodes.java
+++ src/DataStructures/linked_list/Problems/ReverseKNodes.java
@@ -6,6 +6,13 @@ import static DataStructures.linked_list.Node.SinglyLinkedList.node;
 import java.util.Arrays;
 import java.util.stream.IntStream;
 
+/**
+ * Given a linked list 1 2 3 4 5 6 and a number k, reverse every k nodes if exists
+ * if k = 2 then
+ *      2 1 4 3 6 5
+ * if k = 3 then
+ *      3 2 1 6 5 4
+ */
 public class ReverseKNodes {
     public static void main(String[] args) {
         int k = 3;
@@ -23,13 +30,23 @@ public class ReverseKNodes {
         node<T> prev = null;
         node<T> previous = null;
         node<T> next = null;
+
+        // if the current length is not divisible by k then
+        // the last element will be the tail automatically
         boolean isLengthDivisibleByK = (length % k == 0);
         while(length >= k){
             prev = null;
+
+            // getting the kth node and k+1th node
             node<T> kthNode = getNode(curr, k);
             node<T> kthNextNode = kthNode.getNext();
             while(curr != kthNextNode){
+
+                // save the next pointer first
+                // if next is calculated using next.getNext() then nullPointer will be thrown
                 next = curr.getNext();
+
+                // after reversing every k nodes prev will become null
                 if(prev == null){
                     curr.setNext(kthNextNode);
                     if(previous != null) previous.setNext(kthNode);
@@ -40,9 +57,12 @@ public class ReverseKNodes {
                 prev = curr;
                 curr = next;
             }
+
+            // for first iteration the given prev node will become head
             if(length == first.getSize()){
                 first.setHead(prev);
             }
+            // if length is divisible and last iteration then set the tail
             else if(isLengthDivisibleByK && length == k) {
                 first.setTail(previous);
             }
