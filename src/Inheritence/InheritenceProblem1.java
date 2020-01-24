f14c57c0c9d008bc129a1293cca8a6e908ee4983
amanda16 <abhishek_mandal@optum.com>
2020-01-24 11:00:03 +0530

    runtime polymorphism during inheritence 24Jan2020

--- src/Inheritence/InheritenceProblem1.java
+++ src/Inheritence/InheritenceProblem1.java
@@ -1,20 +1,38 @@
 package Inheritence;
 
 
-// static methods cannot be overriden
+/**
+ * when reference variable is of base class and object is allocated using the extended class then
+ *   -  When methods are static then runtime polymorphism does not happen
+ *   -  Runtime polymorphism does happen for instance methods
+ *   -  final methods cannot be overridden
+ */
 class a{
-    public static void hello(){
+    public final void hello(){
+        System.out.println("final hello from a");
+    }
+    public void instanceHello(){
+        System.out.println("instance hello from a");
+    }
+    public static void staticHello(){
         System.out.println("static hello from a");
     }
 }
 
 class b extends a{
-    public static void hello(){
+    public void instanceHello(){
+        System.out.println("instance hello from b");
+    }
+    public static void staticHello(){
         System.out.println("static hello from b");
     }
 }
 public class InheritenceProblem1 {
     public static void main(String[] args) {
-        b.hello();
+
+       a obj = new b();
+       a.staticHello(); // will call static hello a
+       obj.instanceHello(); // will call instance hello b
+       obj.hello();
     }
 }
