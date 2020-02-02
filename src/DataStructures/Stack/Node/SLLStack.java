fe60f9e6d31f4221247c8f97936e3ca4d13c8bba
amanda16 <abhishek_mandal@optum.com>
2020-02-03 02:13:48 +0530

    Stacks problem 1 3Feb2020

--- src/DataStructures/Stack/Node/SLLStack.java
+++ src/DataStructures/Stack/Node/SLLStack.java
@@ -1,9 +1,62 @@
 package DataStructures.Stack.Node;
 
-public class SLLStack<T> {
-    public static class Node<T>{
-        private T data;
-        private Node<T> next;
 
+import java.util.EmptyStackException;
+
+public class SLLStack<T> {
+    private node<T> top;
+    private int size;
+    public static class node<T>{
+        private T data;
+        private node<T> next;
+        public node(T data, node<T> next){
+            this.data = data;
+            this.next = next;
+        }
+        @Override
+        public String toString(){
+            return "[" + this.data + "]";
+        }
+    }
+    public SLLStack(){
+        this.size = 0;
+        this.top = null;
+    }
+
+    public void push(T data){
+        node<T> temp = new node<>(data, top);
+        top = temp;
+        ++this.size;
+    }
+
+    public T peek(){
+        if(isEmpty()) throw new EmptyStackException();
+        return top.data;
+    }
+
+    public T pop(){
+        if(isEmpty()) throw new EmptyStackException();
+        node<T> temp = top;
+        top = top.next;
+        --this.size;
+        return temp.data;
+    }
+
+    public boolean isEmpty(){
+        return this.size == 0;
+    }
+
+    @Override
+    public String toString(){
+        StringBuilder br = new StringBuilder("[");
+        node<T> temp = this.top;
+        while(temp != null){
+            br.append(temp.data);
+            if(temp.next != null)
+                br.append(", ");
+            temp = temp.next;
+        }
+        br.append("]");
+        return br.toString();
     }
 }
