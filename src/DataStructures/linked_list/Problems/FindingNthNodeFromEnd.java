af59df18f49b5f631ca54a50b14cb3f1408ae48c
amanda16 <abhishek_mandal@optum.com>
2020-02-12 17:38:18 +0530

    refactoring logic for nth node from end 12Feb2020

--- src/DataStructures/linked_list/Problems/FindingNthNodeFromEnd.java
+++ src/DataStructures/linked_list/Problems/FindingNthNodeFromEnd.java
@@ -1,29 +1,39 @@
 package DataStructures.linked_list.Problems;
 
 import DataStructures.linked_list.Node.SinglyLinkedList;
+import static DataStructures.linked_list.Node.SinglyLinkedList.node;
 
 import java.util.Scanner;
+/**
+ * Finding Nth node from end in single ll.
+ * Approach:
+ *      Move the first pointer till nth node, then place the second node to the head,
+ *      move both the pointer till first pointer next != null
+ */
 
 public class FindingNthNodeFromEnd {
-    private static <T> SinglyLinkedList.node<?> getNthNodeFromEnd(SinglyLinkedList<?> arr, int n){
-        if(n > arr.getSize()) throw new IllegalStateException("Out of bounds");
-        int nthNode = arr.getSize() - n + 1;
-        SinglyLinkedList.node<?> temp = null;
-        for(int i = 0; i < nthNode; i++){
-            if(temp == null){
-                temp = arr.getHead();
-                continue;
+    private static <T> SinglyLinkedList.node<T> getNthNodeFromEnd2(SinglyLinkedList<T> arr, int n){
+        if(arr == null || arr.getHead() == null) throw new NullPointerException();
+        node<T> first = arr.getHead();
+        int counter = 1;
+        node<T> second = null;
+        while(first.getNext() != null){
+            if(counter == n){
+                second = arr.getHead();
             }
-            temp = temp.getNext();
+            if(second != null) second = second.getNext();
+            first = first.getNext();
+            counter++;
         }
-        return temp;
+        return second;
     }
     public static void main(String[] args) {
         SinglyLinkedList<Integer> arr = new SinglyLinkedList<>();
-        for(int i = 0; i < 10; i++) arr.addLast(i);
+        for(int i = 1; i < 10; i++) arr.addLast(i);
         Scanner s = new Scanner(System.in);
         System.out.println("enter the nth node from the end that you want to find");
         int n = s.nextInt();
-        System.out.println(getNthNodeFromEnd(arr, n));
+        System.out.println(arr);
+        System.out.println(getNthNodeFromEnd2(arr, n));
     }
 }
