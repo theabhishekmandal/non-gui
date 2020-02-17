d6d72e7c7f07c70107ae7757de3767f77e159770
amanda16 <abhishek_mandal@optum.com>
2020-02-17 23:32:06 +0530

    adding comments to KStack problem 16Feb2020

--- src/DataStructures/Stack/Problems/KStack.java
+++ src/DataStructures/Stack/Problems/KStack.java
@@ -2,9 +2,20 @@ package DataStructures.Stack.Problems;
 
 import DataStructures.Stack.Node.kStack;
 
+import java.util.ArrayList;
+
 public class KStack {
     public static void main(String[] args) {
         kStack<Integer> stack = new kStack<>(10, 3);
         stack.push(1, 12);
+        stack.push(1, 14);
+        stack.push(2, 45);
+        stack.push(2, 67);
+
+        stack.pop(1);
+        stack.push(2, 78);
+        System.out.println(stack.toString(1));
+        stack.pop(1);
+        System.out.println(stack.toString(2));
     }
 }
