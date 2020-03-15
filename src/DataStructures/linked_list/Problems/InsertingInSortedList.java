1bb0ccfc77796dba04d9f2ca444300ebfbdc19e5
theabhishekmandal <abhishekmandal0097@gmail.com>
2020-03-15 19:37:59 +0530

    adding inserting node in sorted linked list

--- src/DataStructures/linked_list/Problems/InsertingInSortedList.java
+++ src/DataStructures/linked_list/Problems/InsertingInSortedList.java
@@ -1,29 +1,100 @@
 package DataStructures.linked_list.Problems;
 
+import DataStructures.linked_list.Node.DoublyLinkedList;
 import DataStructures.linked_list.Node.SinglyLinkedList;
 import static DataStructures.linked_list.Node.SinglyLinkedList.node;
 
 import java.util.Random;
 import java.util.stream.IntStream;
 
+/**
+ * Insert a node in a sorted singly linked list and insert a node in doubly linked list
+ */
+
 public class InsertingInSortedList {
     public static void main(String[] args) {
-        Random random = new Random();
-        SinglyLinkedList<Integer> sll = new SinglyLinkedList<>();
-        IntStream.range(1, 11).forEach(sll::addLast);
-        System.out.println("list before insertion "  + sll);
-        insertNodeInSortedList(sll, random.nextInt(10));
-        System.out.println("list before insertion "  + sll);
+        solve();
     }
 
-    private static <T extends Comparable<? super T>> void insertNodeInSortedList(SinglyLinkedList<T> sll, int nextInt) {
-        if(sll == null || sll.getHead() == null) return;
+    private static void solve(){
+        Random random = new Random();
+        SinglyLinkedList<Integer> sll = new SinglyLinkedList<>();
+        DoublyLinkedList<Integer> dll = new DoublyLinkedList<>();
 
-       node<T> temp = sll.getHead();
-       node<T> prev = null;
-       while(temp != null){
+        IntStream.range(1, 11).forEach(sll::addLast);
+        IntStream.range(1, 11).forEach(dll::addLast);
 
-           temp = temp.getNext();
-       }
+        Integer value = random.nextInt(10);
+
+        System.out.println("singly list before insertion "  + sll + "value to be inserted " + value);
+        insertNodeInSortedList(sll, value);
+        System.out.println("singly list after insertion "  + sll);
+
+        System.out.println("doubly list before insertion "  + dll + "value to be inserted " + value);
+        insertNodeInSortedDoublyList(dll, value);
+        System.out.println("doubly list after insertion "  + dll);
+    }
+
+    private static <T extends Comparable<? super T>> void insertNodeInSortedList(SinglyLinkedList<T> sll, T nextValue) {
+        if(sll == null) {
+            sll = new SinglyLinkedList<>();
+        }
+        if(sll.getHead() == null){
+            sll.addLast(nextValue);
+            return;
+        }
+
+        node<T> temp = sll.getHead();
+        node<T> newNode = new node<>(nextValue, null);
+        if(sll.getHead().getData().compareTo(nextValue) > 0){
+            newNode.setNext(sll.getHead());
+            sll.setHead(newNode);
+            return;
+        }
+        while(temp.getNext() != null){
+            if(temp.getNext().getData().compareTo(nextValue) > 0){
+                newNode.setNext(temp.getNext());
+                temp.setNext(newNode);
+                return;
+            }
+            temp = temp.getNext();
+        }
+        temp.setNext(newNode);
+    }
+
+    private static <T extends Comparable<? super T>> void insertNodeInSortedDoublyList(DoublyLinkedList<T> dll, T value) {
+        if(dll == null || dll.getHead() == null) {
+            dll = new DoublyLinkedList<>();
+        }
+        if(dll.getHead() == null){
+            dll.addLast(value);
+            return;
+        }
+
+        DoublyLinkedList.node<T> curr = dll.getHead();
+        DoublyLinkedList.node<T> newNode = new DoublyLinkedList.node<>(null, value, null);
+
+        while(curr.getNext() != null){
+            if(curr.getData().compareTo(value) > 0){
+                DoublyLinkedList.node<T> prev = curr.getPrevious();
+
+                // to check if first element is greater than current
+                if(prev == null){
+                    newNode.setNext(curr);
+                    dll.setHead(newNode);
+                    return;
+                }
+                prev.setNext(newNode);
+                newNode.setPrevious(prev);
+
+                newNode.setNext(curr);
+                curr.setPrevious(newNode);
+                return;
+            }
+            curr = curr.getNext();
+        }
+
+        curr.setNext(newNode);
+        newNode.setPrevious(curr);
     }
 }
