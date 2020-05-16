978a0abffdef2d4cf89ada2d817e6fe06e899bc4
theabhishekmandal <abhishekmandal0097@gmail.com>
2020-05-16 10:02:02 +0530

    temp changes

--- src/Miscellaneous/leetcode/Challenge30DaysApril/Week2/BackSpaceStringCompare.java
+++ src/Miscellaneous/leetcode/Challenge30DaysApril/Week2/BackSpaceStringCompare.java
@@ -4,8 +4,38 @@ package Miscellaneous.leetcode.Challenge30DaysApril.Week2;
 import java.util.Scanner;
 
 /**
+ * Week2 day 2
  * ab#####cd#ba#
  * #####a##b#d
+ * Given two strings S and T, return if they are equal when both are typed into empty text editors. # means a backspace character.
+ *
+ * Note that after backspacing an empty text, the text will continue empty.
+ *
+ * Example 1:
+ *
+ * Input: S = "ab#c", T = "ad#c"
+ * Output: true
+ * Explanation: Both S and T become "ac".
+ * Example 2:
+ *
+ * Input: S = "ab##", T = "c#d#"
+ * Output: true
+ * Explanation: Both S and T become "".
+ * Example 3:
+ *
+ * Input: S = "a##c", T = "#a#c"
+ * Output: true
+ * Explanation: Both S and T become "c".
+ * Example 4:
+ *
+ * Input: S = "a#c", T = "b"
+ * Output: false
+ * Explanation: S becomes "c" while T becomes "b".
+ * Note:
+ *
+ * 1 <= S.length <= 200
+ * 1 <= T.length <= 200
+ * S and T only contain lowercase letters and '#' characters.
  */
 
 public class BackSpaceStringCompare {
@@ -21,28 +51,22 @@ public class BackSpaceStringCompare {
     }
 
     private static boolean solve(String first, String second) {
-        first = getSingleHashString(first);
-        second = getSingleHashString(second);
+        first = solveString(first);
+        second = solveString(second);
         return first.equals(second);
     }
-    private static String getSingleHashString(String string){
-        StringBuilder br =  new StringBuilder();
+    private static String solveString(String string){
+        StringBuilder br = new StringBuilder();
         int hashCounter = 0;
-        for(int i = string.length() - 1 ; i >= 0;){
-            char curr = string.charAt(i);
-            if(curr == '#'){
-                int temp = hashCounter;
-                for(i = i - 1; i >= 0 && string.charAt(i) == '#';){
-                    temp += 1;
-                    i--;
-                }
-                if(i == -1) break;
-                while(i >= 0 && string.charAt(i) != '#');
+        for(int i = string.length() - 1; i >= 0; i--){
+            if(string.charAt(i) == '#'){
+                hashCounter++;
             }
-            else
-                br.append(curr);
-
-            i--;
+            else{
+                if(hashCounter == 0) br.append(string.charAt(i));
+                else
+                    hashCounter--;
+            }
         }
         return br.toString();
     }
---
+++ src/Miscellaneous/leetcode/Challenge30DaysApril/Week2/BinaryTreeDiameter.java
@@ -0,0 +1,116 @@
+package Miscellaneous.leetcode.Challenge30DaysApril.Week2;
+
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+/**
+ * Week 2 day 4
+ *
+ */
+class TreeNode {
+      int val;
+      TreeNode left;
+      TreeNode right;
+      TreeNode() {}
+      TreeNode(int val) { this.val = val; }
+      TreeNode(int val, TreeNode left, TreeNode right) {
+          this.val = val;
+          this.left = left;
+          this.right = right;
+      }
+  }
+public class BinaryTreeDiameter {
+    private TreeNode root;
+    private final Random random;
+    private final StringBuilder stringBuilder;
+    private final List<List<String>> list;
+    public BinaryTreeDiameter(){
+        this.root = null;
+        this.random = new Random();
+        this.stringBuilder = new StringBuilder();
+        this.list = new ArrayList<>(Collections.nCopies(100, new ArrayList<>()));
+    }
+    public void insertInBinaryTreeLevelOrder(Integer data){
+        if(this.root == null) {
+            this.root = new TreeNode(data);
+            return;
+        }
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+
+        while(!queue.isEmpty()){
+            TreeNode temp = queue.poll();
+            boolean leftSide = random.nextBoolean();
+            if(leftSide){
+                if(temp.left != null)
+                    queue.add(temp.left);
+                else{
+                    temp.left = new TreeNode(data);
+                    return;
+                }
+            }
+            else{
+                if(temp.right != null)
+                    queue.add(temp.right);
+                else{
+                    temp.right = new TreeNode(data);
+                    return;
+                }
+            }
+        }
+    }
+    @Override
+    public String toString(){
+        getString(null, this.root, "root", 0);
+        int counter = 0;
+        for(int i = list.size() - 1; i >= 0; i--){
+            List<String> insidelist = list.get(i);
+            if(!insidelist.isEmpty()){
+                String spaces = IntStream.range(0, counter).boxed().map(x -> " ").collect(Collectors.joining());
+                StringBuilder what = new StringBuilder();
+                String answer = String.join(spaces, insidelist);
+                System.out.println(answer);
+                counter++;
+            }
+        }
+        return stringBuilder.toString();
+    }
+
+    private void getString(TreeNode parent, TreeNode node, String r, int stackdepth) {
+        if(node == null) return;
+        getString(node, node.left , "l", stackdepth + 1);
+        String nodevalue = "(" + (parent == null ? "null" : parent.val) + ", " + node.val + ", " + r + ")";
+        stringBuilder.append(nodevalue);
+        List<String> templist = list.get(stackdepth);
+        templist.add(nodevalue);
+//        list.set(stackdepth, (list.get(stackdepth) != null)? list.get(stackdepth) + " " + nodevalue: nodevalue);
+        getString(node, node.right, "r", stackdepth + 1);
+    }
+
+    public static void main(String[] args) {
+        BinaryTreeDiameter ob = new BinaryTreeDiameter();
+        for(int i = 0; i < 20; i++){
+            ob.insertInBinaryTreeLevelOrder(i);
+        }
+        int value = ob.findLongestPath(ob.root);
+        System.out.println(value);
+        ob.toString();
+    }
+
+    private int findLongestPath(TreeNode root) {
+        max = 0;
+        count(root);
+        return max;
+    }
+    private static int max;
+    private int count(TreeNode node) {
+        if(node == null) return 0;
+        else{
+            int leftvalue = count(node.left);
+            int rightvalue = count(node.right);
+            max = Math.max(max, leftvalue + rightvalue);
+            return Math.max(leftvalue, rightvalue) + 1;
+        }
+    }
+}
