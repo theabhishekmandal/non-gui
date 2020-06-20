3b41d090f8f5d4b85df52f190cffb995bdd93074
abhishek <abhishekmandal0097@gmail.com>
2020-06-20 23:23:48 +0530

    CheckSumInTree.java using recursion

--- src/DataStructures/Tree/Problems/CheckSumInTree.java
+++ src/DataStructures/Tree/Problems/CheckSumInTree.java
@@ -12,9 +12,14 @@ import java.util.stream.IntStream;
  * check whether there exists a path from root to any of the nodes.
  *
  * Approach,
- * -    Use any traversal and while pushing the node in the stack or queue, just add (sum - current node value) along with the
- *      node value.
- * -    if leaf node is reach check the added (sum - current node value) is 0, if it is zero then return true otherwise false
+ *  -   Iterative
+ *      -   Use any traversal and while pushing the node in the stack or queue, just add (sum - current node value) along with the
+ *          node value.
+ *      -   if leaf node is reach check the added (sum - current node value) is 0, if it is zero then return true otherwise false
+ *  -   Recursion
+ *      -   In recursion call the root without subtracting the value and then while calling left or right subtree
+ *          , then subtract the value
+ *      -   when the left and right child are null then add the current node and then add it to the static list.
  *
  */
 
@@ -32,7 +37,9 @@ class Pair<A, B> {
     }
 
     public boolean equals(Object other) {
-        return other instanceof Pair && Objects.equals(this.fst, ((Pair<A, B>)other).fst) && Objects.equals(this.snd, ((Pair<A, B>)other).snd);
+        return other instanceof Pair &&
+                Objects.equals(this.fst, ((Pair<A, B>)other).fst) &&
+                Objects.equals(this.snd, ((Pair<A, B>)other).snd);
     }
 
     public int hashCode() {
@@ -52,17 +59,33 @@ public class CheckSumInTree {
         Random random = new Random();
         BinaryTree<Integer> binaryTree = new BinaryTree<>();
         IntStream.range(0, random.nextInt(20)).forEach(binaryTree::insertInBinaryTreeLevelOrder);
-        int number = random.nextInt(100);
+        int number = random.nextInt(20);
         System.out.println("Number to be found " + number + "\n" + binaryTree.levelOrder());
+
+        // Using Iteration
         boolean isSumExists = checkSumInTree(binaryTree.getRoot(), number);
         if(isSumExists){
             System.out.println(finalList);
             finalList.clear();
         }
-        else
+        else{
             System.out.println("no path exists");
+        }
+
+
+        // Using Recursion
+        checkSumInTreeRecursion(binaryTree.getRoot(), number, "[");
+        if(finalListRecursion.isEmpty()){
+            System.out.println("no path exists");
+        }
+        else{
+            System.out.println(finalListRecursion);
+            finalListRecursion.clear();
+        }
     }
+
     private static final List<String> finalList = new ArrayList<>();
+
     private static boolean checkSumInTree(node<Integer> root, int sum) {
         if(root == null) return false;
 
@@ -92,5 +115,31 @@ public class CheckSumInTree {
            }
         }
         return found;
+    }
+
+    private static final List<String> finalListRecursion = new ArrayList<>();
+    private static void checkSumInTreeRecursion(node<Integer> node, int sum, String ans){
+        if(node == null){
+            return;
+        }
+
+        Integer data = node.getData();
+        if(node.getLeft() == null && node.getRight() == null && sum - data == 0){
+            finalListRecursion.add(ans + ", " + node.getData().toString() + " ]");
+            return;
+        }
+
+        // checking for first time
+        String temp = ans;
+        if(ans.length() == 1) {
+            temp += " ";
+        }
+        else{
+           temp += ", ";
+        }
+        temp += node.getData().toString();
+
+        checkSumInTreeRecursion(node.getLeft(), sum - node.getData(), temp);
+        checkSumInTreeRecursion(node.getRight(), sum - node.getData(), temp);
     }
 }
