1a67be08cd00bb715eb7309a01820f7bcae71a45
theabhishekmandal <abhishekmandal0097@gmail.com>
2021-05-14 21:23:49 +0530

    adding kth ancestor of a node

--- src/data_structures/tree/binary_tree/problems/KthAncestorOfANode.java
+++ src/data_structures/tree/binary_tree/problems/KthAncestorOfANode.java
@@ -1,34 +1,39 @@
 package data_structures.tree.binary_tree.problems;
 
 import data_structures.tree.binary_tree.binary_tree_impl.BinaryTree;
-import static data_structures.tree.binary_tree.binary_tree_impl.BinaryTree.Node;
 
 import java.util.*;
-import java.util.stream.IntStream;
 
-public class KthAncestorOfANodes {
+import static data_structures.tree.binary_tree.binary_tree_impl.BinaryTree.Node;
+
+/**
+ * Given a binary tree in which nodes are numbered from 1 to n. We have to find the kth ancestor of the node.
+ *
+ * Approach:
+ *  - Add all the path from root to that node, and then traverse from last to find the kth ancestor
+ *
+ */
+public class KthAncestorOfANode {
     public static void main(String[] args) {
         var random = new Random();
         var binaryTree = new BinaryTree<Integer>();
-        random.ints(0, 10)
+        random.ints(0, 20)
+                .distinct()
                 .limit(10)
                 .forEach(binaryTree::insertInBinaryTreeLevelOrder);
-        int first = random.nextInt(10);
-        int second = random.nextInt(10);
-        int kthAncestor = 1 + random.nextInt(3);
+        var nodeToBeFound = random.nextInt(10);
+        var kthAncestor = 1 + random.nextInt(3);
 
-        System.out.println("first = " + first + " second = " + second + " kthAncestor = " + kthAncestor);
+        System.out.println("nodes ancestor to be found = " + nodeToBeFound);
         System.out.println(binaryTree.levelOrderPretty());
-        System.out.println(getKthAncestor(binaryTree, first, second, kthAncestor));
+        System.out.println(kthAncestor + " ancestor of node = " + nodeToBeFound + " is = " + getKthAncestor(binaryTree, nodeToBeFound, kthAncestor));
     }
 
-    private static Integer getKthAncestor(BinaryTree<Integer> binaryTree, int first, int second, int kthAncestor) {
-        List<Node<Integer>> firstList = getKthAncestorNode(binaryTree.getRoot(), first);
-        List<Node<Integer>> secondList = getKthAncestorNode(binaryTree.getRoot(), second);
-        System.out.println(firstList + "\n" + secondList);
-        int length = Math.min(firstList.size(), secondList.size());
-        if (length >= kthAncestor) {
-            return firstList.get(length - kthAncestor).getData();
+    private static Integer getKthAncestor(BinaryTree<Integer> binaryTree, int nodeTobeFound, int kthAncestor) {
+        var list = getKthAncestorNode(binaryTree.getRoot(), nodeTobeFound);
+        var length = list.size();
+        if (length > kthAncestor) {
+            return list.get(length - kthAncestor - 1).getData();
         }
         return -1;
     }
@@ -38,7 +43,7 @@ public class KthAncestorOfANodes {
         List<Node<Integer>> ancestorNodeList = new ArrayList<>();
         stack.push(root);
         stack.push(root);
-        boolean found = false;
+        var found = false;
         while (!stack.isEmpty()) {
             var curr = stack.pop();
             if (!stack.isEmpty() && curr == stack.peek()) {
