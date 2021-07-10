eba500a39191348f0e4e40b57bd62962a1b52b06
theabhishekmandal <abhishekmandal0097@gmail.com>
2021-07-10 16:02:42 +0530

    Enum and Autoboxing changes

--- src/enumeration_and_autoboxing/AutoboxingDemo.java
+++ src/enumeration_and_autoboxing/AutoboxingDemo.java
@@ -4,8 +4,8 @@ import java.util.*;
 
 public class AutoboxingDemo {
     public static void main(String[] args) {
-        Scanner s = new Scanner(System.in);
-        int in = s.nextInt(); // Auto box an int
+        var s = new Scanner(System.in);
+        var in = s.nextInt(); // Auto box an int
         Integer n = in; //Auto unbox an int
         System.out.println(n);
     }
