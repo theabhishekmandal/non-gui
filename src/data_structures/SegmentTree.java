74ecca565c4a99601c84c9c9551a69c92959699e
abhishek <abhishekmandal0097@gmail.com>
2021-09-27 23:38:21 +0530

    adding segmentTree refactoring

--- src/data_structures/SegmentTree.java
+++ src/data_structures/SegmentTree.java
@@ -4,70 +4,90 @@ import java.util.Arrays;
 
 import static java.lang.System.err;
 import static java.lang.System.out;
-public class SegmentTree{
-    static int[] segmentTree;
-    SegmentTree(int[] arr){
-        int x = (int)Math.ceil(Math.log(arr.length - 1) / Math.log(2));
-        int maxsize = 2 * (int)Math.pow(2, x) - 1;
+
+public class SegmentTree {
+    private final int[] segmentTree;
+
+    public SegmentTree(int[] arr) {
+        final int x = (int) Math.ceil(Math.log(arr.length - 1) / Math.log(2));
+        final int maxsize = 2 * (int) Math.pow(2, x) - 1;
         segmentTree = new int[maxsize];
-        createtree(arr, 0, arr.length - 1, 0);
+        createTree(arr, 0, arr.length - 1, 0);
     }
-    private  int getmid(int one, int two){
+
+    private int getMid(int one, int two) {
         return one + (two - one) / 2;
     }
-    private int createtree(int[] arr, int start, int end, int index) {
-        if(start == end){
+
+    private int createTree(int[] arr, int start, int end, int index) {
+        if (start == end) {
             segmentTree[index] = arr[start];
             return segmentTree[index];
         }
-        int mid = getmid(start, end);
-        segmentTree[index] = createtree(arr, start, mid, 2 * index + 1) +
-                    createtree(arr, mid + 1, end, 2 * index + 2);
+        int mid = getMid(start, end);
+        segmentTree[index] = createTree(arr, start, mid, getLeft(index)) +
+                createTree(arr, mid + 1, end, getRight(index));
         return segmentTree[index];
     }
-    private int getsumcheck(int segmentstart, int segmentend, int arraystart, int arrayend, int index){
-        if(arraystart <= segmentstart && arrayend >= segmentend){
+
+    private int getSumCheck(int segmentStart, int segmentEnd, int arrayStart, int arrayEnd, int index) {
+        if (arrayStart <= segmentStart && arrayEnd >= segmentEnd) {
             return segmentTree[index];
         }
-        if(arraystart > segmentend || segmentstart > arrayend)
+        if (arrayStart > segmentEnd || segmentStart > arrayEnd) {
             return 0;
-        int mid = getmid(segmentstart, segmentend);
-        return getsumcheck(segmentstart, mid, arraystart, arrayend, 2 * index + 1) +
-                getsumcheck(mid + 1, segmentend, arraystart, arrayend, 2 * index + 2);
+        }
+        final int mid = getMid(segmentStart, segmentEnd);
+        return getSumCheck(segmentStart, mid, arrayStart, arrayEnd, getLeft(index)) +
+                getSumCheck(mid + 1, segmentEnd, arrayStart, arrayEnd, getRight(index));
     }
-    private int getsum(int[] arr, int arraystart, int arrayend){
-        if(arraystart < 0 ||  arrayend > arr.length - 1 ||  arrayend < arraystart){
+
+    private int getSum(final int[] arr, final int arrayStart, final int arrayEnd) {
+        if (arrayStart < 0 || arrayEnd > arr.length - 1 || arrayEnd < arrayStart) {
             return -1;
         }
-        return getsumcheck( 0, arr.length - 1, arraystart, arrayend, 0);
+        return getSumCheck(0, arr.length - 1, arrayStart, arrayEnd, 0);
     }
-    private void printArray(){
+
+    private void printArray() {
         out.println(Arrays.toString(segmentTree));
     }
-    private void updateutil(int segementstart, int segementend, int arrayindex, int diff, int segindex){
-        if(segementend < arrayindex || segementstart > arrayindex)
-            return ;
-        segmentTree[segindex] += diff;
-        if(segementstart != segementend){
-            int mid = getmid(segementstart, segementend);
-            updateutil(segementstart, mid, arrayindex, diff, 2 * segindex + 1);
-            updateutil(mid + 1, segementend, arrayindex, diff, 2 * segementend + 2);
+
+    private void updateUtil(int segmentStart, int segmentEnd, int arrayIndex, int diff, int segIndex) {
+        if (segmentEnd < arrayIndex || segmentStart > arrayIndex) {
+            return;
+        }
+        segmentTree[segIndex] += diff;
+        if (segmentStart != segmentEnd) {
+            final int mid = getMid(segmentStart, segmentEnd);
+            updateUtil(segmentStart, mid, arrayIndex, diff, 2 * segIndex + 1);
+            updateUtil(mid + 1, segmentEnd, arrayIndex, diff, 2 * segmentEnd + 2);
         }
     }
-    private void update(int[] arr, int index, int val){
-        if(index < 0 || index > arr.length - 1){
+
+    private void update(int[] arr, int index, int val) {
+        if (index < 0 || index > arr.length - 1) {
             err.println("Invalid index");
             return;
         }
-        int diff = val - arr[index];
+        final int diff = val - arr[index];
         arr[index] = val;
-        updateutil( 0, arr.length - 1, index, diff, 0);
+        updateUtil(0, arr.length - 1, index, diff, 0);
     }
+
+    private static int getLeft(int index) {
+        return 2 * index + 1;
+    }
+
+    private static int getRight(int index) {
+        return 2 * index + 2;
+    }
+
     public static void main(String[] args) {
-        int arr[] = {1, 3, 5, 7 , 9, 11};
-        SegmentTree ob = new SegmentTree(arr);
+        final int[] arr = {1, 3, 5, 7, 9, 11};
+        final SegmentTree ob = new SegmentTree(arr);
         ob.printArray();
-        out.println(ob.getsum(arr, 0 , 1));
+        out.println(ob.getSum(arr, 0, 1));
         ob.update(arr, 0, 14);
         out.println(Arrays.toString(arr));
         ob.printArray();
