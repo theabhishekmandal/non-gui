eb2e0f25c176685511b685a6a4d7a022e7877fbc
abhishek <abhishekmandal0097@gmail.com>
2022-02-24 10:31:03 +0530

    Multithreading example

---
+++ src/multi_threading/problems/HackerThreadCaseStudy.java
@@ -0,0 +1,108 @@
+package multi_threading.problems;
+
+import java.util.List;
+import java.util.Random;
+
+public class HackerThreadCaseStudy {
+    public static void main(String[] args) {
+        Vault vault = new Vault(new Random().nextInt(1000));
+        List<Thread> threadList = List.of(
+                new AscendingThread(vault),
+                new DescendingThread(vault),
+                new PoliceThread()
+        );
+
+        threadList.forEach(Thread::start);
+
+    }
+}
+
+class Vault {
+    private final int passWord;
+
+    public Vault(int passWord) {
+        this.passWord = passWord;
+    }
+
+    public boolean isCorrectPassword(int guessedPassword) {
+        try {
+            Thread.sleep(5);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        return passWord == guessedPassword;
+    }
+}
+
+abstract class HackerThread extends Thread {
+    protected Vault vault;
+
+    protected HackerThread(Vault vault) {
+        this.vault = vault;
+        this.setName(this.getClass().getSimpleName());
+        this.setPriority(MAX_PRIORITY);
+    }
+
+    @Override
+    public synchronized void start() {
+        System.out.println("Starting thread " + this.getName());
+        super.start();
+    }
+}
+
+
+class AscendingThread extends HackerThread {
+
+    protected AscendingThread(Vault vault) {
+        super(vault);
+    }
+
+    @Override
+    public void run() {
+        for (int i = 0; i < Integer.MAX_VALUE; i++) {
+            if (super.vault.isCorrectPassword(i)) {
+                System.out.println("guessed password is " + i);
+                System.exit(0);
+            }
+        }
+    }
+}
+
+
+class DescendingThread extends HackerThread {
+
+    protected DescendingThread(Vault vault) {
+        super(vault);
+    }
+
+    @Override
+    public void run() {
+        for (int i = Integer.MAX_VALUE; i >= 0; i--) {
+            if (super.vault.isCorrectPassword(i)) {
+                System.out.println("guessed password is " + i);
+                System.exit(0);
+            }
+        }
+    }
+}
+
+class PoliceThread extends Thread {
+
+    @Override
+    public void run() {
+        for (int i = 0; i < 20; i++) {
+            System.out.println(i);
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        System.out.println("Game over hackers");
+        System.exit(0);
+    }
+}
+
+
+
+
