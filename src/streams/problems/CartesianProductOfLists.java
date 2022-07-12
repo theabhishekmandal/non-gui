547c54861f32fc3c56e10ff697d6151e8e0cdcc5
abhishek <abhishekmandal0097@gmail.com>
2022-07-12 23:02:03 +0530

    Reduce Operation

--- src/streams/problems/CartesianProductOfLists.java
+++ src/streams/problems/CartesianProductOfLists.java
@@ -1,6 +1,8 @@
 package streams.problems;
 
+import java.util.Arrays;
 import java.util.List;
+import java.util.function.BinaryOperator;
 import java.util.function.Supplier;
 import java.util.stream.Stream;
 
@@ -12,18 +14,35 @@ public class CartesianProductOfLists {
     public static void main(String[] args) {
         Supplier<Stream<String>> s1 = () -> Stream.of("Lawful", "Neutral", "Chaotic");
         Supplier<Stream<String>> s2 = () -> Stream.of("Good", "Neutral", "Evil");
+        Supplier<Stream<String>> s3 = () -> Stream.of("Simple", " ", "Complex");
 
-        Supplier<Stream<String>> reduced = () -> s1.get().flatMap(a -> s2.get().map(b -> a + b));
-        reduced.get().forEach(System.out::println);
+        // The idea is to take two supplier stream and combine them again and again
+        BinaryOperator<Supplier<Stream<String>>> binaryOperator = (x, y) -> () -> x.get().flatMap(a -> y.get().map(b -> a + b));
+        Supplier<Stream<String>> result = null;
+        boolean notFound = false;
+        for (var supplier : Arrays.asList(s1, s2, s3)) {
+            if (!notFound) {
+                notFound = true;
+                result = supplier;
+            } else {
+                result = binaryOperator.apply(result, supplier);
+            }
+        }
+        result.get().forEach(System.out::println);
 
+
+
+        // Now the above thing in more concise manner
         List<List<String>> input = List.of(
                 List.of("Lawful ", "Neutral ", "Chaotic "),
                 List.of("Simple ", " ", "Complex "),
                 List.of("Good", "Neutral", "Evil")
         );
 
+
         System.out.println();
-        Stream<Supplier<Stream<String>>> streamOfStreamOfSuppliers = input.stream().map(list -> list::stream);
+        // First Create a supplier of streams, so that our internal list is consumable again
+        Stream<Supplier<Stream<String>>> streamOfStreamOfSuppliers = input.stream().map(list -> () -> list.stream());
         Supplier<Stream<String>> s = streamOfStreamOfSuppliers
                 .reduce((x, y) -> () -> x.get().flatMap(a -> y.get().map(b -> a + b)))
                 .orElse(() -> Stream.of(""));
